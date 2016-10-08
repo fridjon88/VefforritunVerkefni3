1a98594d8a71247b21da478393a2aa33d4eb61a9
fridjon88 <fridjon88@gmail.com>
2016-10-08 22:42:16 +0000

    final

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded',function() {
-  Forum.init();
+document.addEventListener('DOMContentLoaded', function() {
+    Forum.init();
 });
 
 /**
@@ -18,301 +18,290 @@ document.addEventListener('DOMContentLoaded',function() {
  */
 var Forum = (function() {
 
-  /**
-   * Lykill fyrir localstorage, geymum *allar* færslur undir þessum lykli í
-   * fylki sem er breytt í JSON streng með JSON.stringify() og til baka í fylki
-   * með JSON.parse()
-   */
-  var LOCALSTORAGE = 'forum';
+    /**
+     * Lykill fyrir localstorage, geymum *allar* færslur undir þessum lykli í
+     * fylki sem er breytt í JSON streng með JSON.stringify() og til baka í fylki
+     * með JSON.parse()
+     */
+    var LOCALSTORAGE = 'forum';
 
-  /**
-   * Inngangur í forrit, viljum:
-   *  1. Athuga hvort við eigum vistuð gögn í localStorage, ef svo er,
-   *     hlaða þeim inn (yfir allt sem er til staðar nú þegar)
-   *  2. Ef engin gögn, finna gögn sem eru nú þegar til staðar og vista þau í
-   *     localStorage
-   *  3. Binda event handlera við nýja færslu form og nýtt svar form
-   *  4. Binda "takmarkara" við alla reiti
-   *
-   * Þegar ný færsla eða nýtt svar er búið til, er góð leið að finna _allar_
-   * færslur og svör úr DOM og vista það, getum þá samnýtt fall úr 1. í það og
-   * losnum við tvöfalt bókhald, eitt í DOM og annað í kóða.
-   */
-  function init() {
-    console.log('Halló heimur!');
-    var form = document.getElementById('form');
-    var button = form.querySelector('button');
-    var list = document.querySelector('.list');
+    /**
+     * Inngangur í forrit, viljum:
+     *  1. Athuga hvort við eigum vistuð gögn í localStorage, ef svo er,
+     *     hlaða þeim inn (yfir allt sem er til staðar nú þegar)
+     *  2. Ef engin gögn, finna gögn sem eru nú þegar til staðar og vista þau í
+     *     localStorage
+     *  3. Binda event handlera við nýja færslu form og nýtt svar form
+     *  4. Binda "takmarkara" við alla reiti
+     *
+     * Þegar ný færsla eða nýtt svar er búið til, er góð leið að finna _allar_
+     * færslur og svör úr DOM og vista það, getum þá samnýtt fall úr 1. í það og
+     * losnum við tvöfalt bókhald, eitt í DOM og annað í kóða.
+     */
+    function init() {
 
-    console.log('fann form', form, 'með button', button);
+        var form = document.getElementById('form');
+        var button = form.querySelector('button');
+        var list = document.querySelector('.list');
+        form.addEventListener('submit', newEntryHandler);
 
-    form.addEventListener('submit', newEntryHandler);
+        //add posts
+        list.appendChild(addEntry('Jean-Luc Picard', 'The Prime Directive is not just a set of rules; it is a philosophy ... and a very correct one. History has proven again and again that whenever mankind interferes with a less developed civilization, no matter how well intentioned that interference may be, the results are invariably disastrous.'));
+        // var replysNode = document.createElement('li');
+        // replysNode.classList.add('item', 'item-new', 'list-group-item');
+        // var replysNode = form.querySelector('.form');
+        addReply(list, 'Lt. Cmdr. Data', 'We are more alike than unlike, my dear Captain. I have pores. Humans have pores. I have... fingerprints. Humans have fingerprints. My chemical nutrients are like your blood. If you prick me ... do I not ... leak?');
 
-    //add posts
-    list.appendChild(addEntry('Jean-Luc Picard', 'The Prime Directive is not just a set of rules; it is a philosophy ... and a very correct one. History has proven again and again that whenever mankind interferes with a less developed civilization, no matter how well intentioned that interference may be, the results are invariably disastrous.'));
-    // var replysNode = document.createElement('li');
-    // replysNode.classList.add('item', 'item-new', 'list-group-item');
-    // var replysNode = form.querySelector('.form');
-    addReply(list,'Lt. Cmdr. Data', 'We are more alike than unlike, my dear Captain. I have pores. Humans have pores. I have... fingerprints. Humans have fingerprints. My chemical nutrients are like your blood. If you prick me ... do I not ... leak?');
-
-  }
-
-  /**
-   * Finnur núverandi færslur á síðu og skilar í gagnastrúktúr
-   *
-  * @return {array} Fylki af færslum á núverandi síðu í gagnastrúktúr
-   */
-  function parseEntries() {
-  }
-
-  /**
-   * Finnur svör við færslum úr DOMi
-   *
-   * @param {node} element DOM element fyrir færslum
-   *
-   * @return {array} Fylki af svörum, tómt ef engin svör
-   */
-  function parseReplies(element) {
-  }
-
-  /**
-   * Event handler fyrir nýja færslu. Bætir nýrri færslu við DOM.
-   * Setur nafn í alla aðra input reiti
-   *
-   * @param {object} e Event frá formi
-   */
-  function newEntryHandler(e) {
-    console.log('Submit á formi, event er', e);
-
-    e.preventDefault();
-
-    var form = e.target;
-    var nameEntry = form.querySelector('input[name=name]');
-    var textEntry = form.querySelector('textarea[name=comment]');
-
-    var name = nameEntry ? nameEntry.value : '';
-    var text = textEntry ? textEntry.value : '';
-
-    console.log('nafn er', name);
-    console.log('texti er', text);
-
-    var node = addEntry(name, text);
-
-    var list = document.querySelector('.list');
-    console.log('Bætum', node, 'við', list);
-
-    list.appendChild(node);
-
-    nameEntry.value = '';
-    textEntry.value = '';
-  }
-
-  /**
-   * Event handler fyrir nýtt svar. Bætir nýrri færslu við DOM útfrá e.target
-   * Setur nafn í alla aðra input reiti
-   *
-   * @param {object} e Event frá formi
-   */
-  function newReplyHandler(e) {
-    console.log('Submit á reply formi, event er', e);
-
-    e.preventDefault();
-
-    var form = e.target;
-    var nameEntry = form.querySelector('input[name=name]');
-    var textEntry = form.querySelector('input[name=comment]');
-
-    var name = nameEntry ? nameEntry.value : '';
-    var text = textEntry ? textEntry.value : '';
-
-    console.log('nafn er', name);
-    console.log('texti er', text);
-
-
-    var list = document.querySelector('.list');
-    addReply(list,name, text);
-//ATH ATH WORKIN ON THIS 23 53 3 10 2016
-    textEntry.addEventListener(submit ,constrainText.bind(null, 100));
-
-    // console.log('Bætum', replysNode, 'við', list);
-
-    // list.appendChild(replysNode);
-
-    nameEntry.value = '';
-    textEntry.value = '';
-  }
-
-  /**
-   * Event handler fyrir skrif í box, leyfir ekki að skrifa ef stafir eru fleiri
-   * en maxChars og birtir þá skilaboð um það.
-   * Hint: Getum notað constrainText.bind(null, 100) í addEventListener
-   *
-   * @param {number} maxChars Hámark stafa sem má vera
-   * @param {object} e Event frá formi
-   */
-  function constrainText(maxChars, e) {
-  }
-
-  /**
-   * Bætir nýju svari við DOM undir repliesNode
-   *
-   * @param {string} name Nafn fyrir svar
-   * @param {string} text Texti á svar
-   */
-  function addReply(repliesNode, name, text) {
-    var replyNode = document.createElement('div');
-    replyNode.classList.add('reply', 'well');
-
-    var nameNode = document.createElement('h4');
-    nameNode.classList.add('name');
-    nameNode.appendChild(document.createTextNode(name));
-
-    var textNode = document.createElement('p');
-    textNode.classList.add('text');
-    textNode.appendChild(document.createTextNode(text));
-
-    var unorderedList = document.querySelector('ul');
-
-    replyNode.appendChild(nameNode);
-    replyNode.appendChild(textNode);
-
-    repliesNode.appendChild(replyNode);
-  }
-
-  function setName(name) {
-    var nameInputs = document.querySelectorAll('[name="name"]');
-
-    for(var i = 0; i<nameInputs.length; i++) {
-      nameInputs[i].value = name;
     }
-  }
 
-  /**
-   * Bætir nýrri færslu við DOM
-   *
-   * @param {string} name Nafn fyrir færslu
-   * @param {string} text Texti á færslu
-   */
-  function addEntry(name, text) {
-      var entry = document.createElement('li');
-      entry.classList.add('item', 'item-new', 'list-group-item');
-      // entry.dataset.date = date;
+    /**
+     * Finnur núverandi færslur á síðu og skilar í gagnastrúktúr
+     *
+     * @return {array} Fylki af færslum á núverandi síðu í gagnastrúktúr
+     */
+    function parseEntries() {}
 
-      var nameNode = document.createElement('h2');
-      nameNode.classList.add('name');
-      nameNode.appendChild(document.createTextNode(name));
+    /**
+     * Finnur svör við færslum úr DOMi
+     *
+     * @param {node} element DOM element fyrir færslum
+     *
+     * @return {array} Fylki af svörum, tómt ef engin svör
+     */
+    function parseReplies(element) {}
 
-      var textNode = document.createElement('div');
-      textNode.classList.add('text');
-      textNode.appendChild(document.createTextNode(text));
-
-      var replyNode = document.createElement('div');
-      replyNode.classList.add('replyNode', 'reply');
-      replyNode.appendChild(createReplyForm());
-
-
-      entry.appendChild(nameNode);
-      entry.appendChild(textNode);
-      entry.appendChild(replyNode);
+    /**
+     * Event handler fyrir nýja færslu. Bætir nýrri færslu við DOM.
+     * Setur nafn í alla aðra input reiti
+     *
+     * @param {object} e Event frá formi
+     */
+    function newEntryHandler(e) {
 
 
+        e.preventDefault();
 
-      return entry;
-  }
+        var form = e.target;
+        var nameEntry = form.querySelector('input[name=name]');
+        var textEntry = form.querySelector('textarea[name=comment]');
 
-  /**
-   * Býr til DOM tré fyrir reply form. Nýtir createFormGroup fall til að búa til
-   * input, bætir við takka með icon.
-   * Hint: Til að laga spacing mál er nóg að bæta við ' ' á eftir form-group
-   *
-   * @return {node} DOM node fyrir reply form
-   */
-  function createReplyForm() { //work in progress
-    var entry = document.createElement('form');
-    entry.classList.add('item','item-new', 'list-group-item','form-group', 'container-fluid');
-
-    // var nameNode = document.createElement('h2');
-    // nameNode.classList.add('name');
-    // nameNode.appendChild(document.createTextNode(''));
+        var name = nameEntry ? nameEntry.value : '';
+        var text = textEntry ? textEntry.value : '';
 
 
-    var inputNodeName = document.createElement('input');
-    inputNodeName.className = 'name';
-    inputNodeName.name = 'name';
-    // inputNodeName.class.add('form-control', 'form-inline');
-    inputNodeName.placeholder = 'Nafn';
-    inputNodeName.classList.add('name','form-inline', 'col-sm-3', 'col-xs-5');
-    inputNodeName.appendChild(document.createTextNode(''));
+        var node = addEntry(name, text);
 
-    var inputNodeReply = document.createElement('input');
-    inputNodeReply.className = 'comment';
-    inputNodeReply.name = 'comment';
-    inputNodeReply.placeholder = 'Svar';
-    inputNodeReply.classList.add('reply', 'col-sm-5', 'col-sm-offset-1', 'col-xs-6', 'col-xs-offset-1');
-    inputNodeReply.appendChild(document.createTextNode(''));
-
-    var buttonNode = document.createElement('button');
-    buttonNode.className = 'submitButton';
-    buttonNode.type = 'submit';
-    buttonNode.name = 'submitButton';
-    buttonNode.classList.add('btn', 'btn-primary', 'glyphicon', 'glyphicon-pencil','col-sm-2','col-sm-offset-1', 'col-xs-5','col-xs-offset-3');
-    buttonNode.appendChild(document.createTextNode('Svara'));
-    console.log('Bjó til', buttonNode, inputNodeReply);
-
-    entry.addEventListener('submit', newReplyHandler);
+        var list = document.querySelector('.list');
 
 
-    // entry.appendChild(nameNode);
-    entry.appendChild(inputNodeName);
-    entry.appendChild(inputNodeReply);
-    entry.appendChild(buttonNode);
+        list.appendChild(node);
 
-    // entry.appendChild(replysNode);
+        nameEntry.value = '';
+        textEntry.value = '';
+    }
 
-    return entry;
-    // console.log('create reoky form');
-    // var entry = document.createElement('form');
-    // entry.classList.add('reply');
-    //
-    // var nameInputNode = document.createElement('input');
-    // nameInputNode.classList.add('name');
-    // document.getElementsByName('name').placeholder = 'Nafn';
-    //
-    // return entry;
-  }
+    /**
+     * Event handler fyrir nýtt svar. Bætir nýrri færslu við DOM útfrá e.target
+     * Setur nafn í alla aðra input reiti
+     *
+     * @param {object} e Event frá formi
+     */
+    function newReplyHandler(e) {
 
-  /**
-   * Býr til DOM tré fyrir form group.
-   *
-   * @param {string} label Label á form
-   * @param {string} name Nafn á input
-   *
-   * @return {node} DOM node fyrir form group
-   */
-  function createFormGroup(label, name) {
-  }
 
-  /**
-   * Sækir færslur úr localStorage og skilar í fylki
-   *
-   * @param {string} key Lykill sem við notum í localStorage
-   * @param {array} Fylki af færslum til að vista
-   */
-  function save(key, items) {
-  }
+        e.preventDefault();
 
-  /**
-   * Sækir færslur úr localStorage og skilar í fylki
-   *
-   * @param {string} key Lykill sem við notum í localStorage
-   *
-   * @return {array} Fylki af færslum í localStorage ef einhverjar
-   * @return {null} Ef engin færsla til staðar í localStorage
-   */
-  function load(key) {
-  }
+        var form = e.target;
+        var nameEntry = form.querySelector('input[name=name]');
+        var textEntry = form.querySelector('input[name=comment]');
 
-  return {
-    init: init
-  };
+        var name = nameEntry ? nameEntry.value : '';
+        var text = textEntry ? textEntry.value : '';
+
+
+
+        var list = document.querySelector('.list');
+
+
+        addReply(list, name, text);
+        //ATH ATH WORKIN ON THIS 23 53 3 10 2016
+        textEntry.addEventListener(submit, constrainText.bind(null, 100));
+
+        // console.log('Bætum', replysNode, 'við', list);
+
+        // list.appendChild(replysNode);
+
+        nameEntry.value = '';
+        textEntry.value = '';
+    }
+
+    /**
+     * Event handler fyrir skrif í box, leyfir ekki að skrifa ef stafir eru fleiri
+     * en maxChars og birtir þá skilaboð um það.
+     * Hint: Getum notað constrainText.bind(null, 100) í addEventListener
+     *
+     * @param {number} maxChars Hámark stafa sem má vera
+     * @param {object} e Event frá formi
+     */
+    function constrainText(maxChars, e) {}
+
+    /**
+     * Bætir nýju svari við DOM undir repliesNode
+     *
+     * @param {string} name Nafn fyrir svar
+     * @param {string} text Texti á svar
+     */
+    function addReply(repliesNode, name, text) {
+        var replyNode = document.createElement('div');
+        replyNode.classList.add('reply', 'well');
+
+        var nameNode = document.createElement('h4');
+        nameNode.classList.add('name');
+        nameNode.appendChild(document.createTextNode(name));
+
+        var textNode = document.createElement('p');
+        textNode.classList.add('text');
+        textNode.appendChild(document.createTextNode(text));
+
+
+        replyNode.appendChild(nameNode);
+        replyNode.appendChild(textNode);
+
+        repliesNode.appendChild(replyNode);
+    }
+
+    function setName(name) {
+        var nameInputs = document.querySelectorAll('[name="name"]');
+
+        for (var i = 0; i < nameInputs.length; i++) {
+            nameInputs[i].value = name;
+        }
+    }
+
+    /**
+     * Bætir nýrri færslu við DOM
+     *
+     * @param {string} name Nafn fyrir færslu
+     * @param {string} text Texti á færslu
+     */
+    function addEntry(name, text) {
+        var entry = document.createElement('li');
+        entry.classList.add('item', 'item-new', 'list-group-item');
+        // entry.dataset.date = date;
+
+
+        var nameNode = document.createElement('h2');
+        nameNode.classList.add('name');
+        nameNode.appendChild(document.createTextNode(name));
+
+        var textNode = document.createElement('div');
+        textNode.classList.add('text');
+        textNode.appendChild(document.createTextNode(text));
+
+        var replyNode = document.createElement('div');
+        replyNode.classList.add('replyNode', 'reply');
+        replyNode.appendChild(createReplyForm());
+
+
+        entry.appendChild(nameNode);
+        entry.appendChild(textNode);
+        entry.appendChild(replyNode);
+
+
+
+        return entry;
+    }
+
+    /**
+     * Býr til DOM tré fyrir reply form. Nýtir createFormGroup fall til að búa til
+     * input, bætir við takka með icon.
+     * Hint: Til að laga spacing mál er nóg að bæta við ' ' á eftir form-group
+     *
+     * @return {node} DOM node fyrir reply form
+     */
+    function createReplyForm() { //work in progress
+        var entry = document.createElement('form');
+        entry.classList.add('item', 'item-new', 'replyForm', 'list-group-item', 'form-group', 'container-fluid');
+
+        // var nameNode = document.createElement('h2');
+        // nameNode.classList.add('name');
+        // nameNode.appendChild(document.createTextNode(''));
+
+
+        var inputNodeName = document.createElement('input');
+        inputNodeName.className = 'name';
+        inputNodeName.name = 'name';
+        // inputNodeName.class.add('form-control', 'form-inline');
+        inputNodeName.placeholder = 'Nafn';
+        inputNodeName.classList.add('name', 'form-inline', 'col-sm-3', 'col-xs-5');
+        inputNodeName.appendChild(document.createTextNode(''));
+
+        var inputNodeReply = document.createElement('input');
+        inputNodeReply.className = 'comment';
+        inputNodeReply.name = 'comment';
+        inputNodeReply.placeholder = 'Svar';
+        inputNodeReply.classList.add('reply', 'col-sm-5', 'col-sm-offset-1', 'col-xs-6', 'col-xs-offset-1');
+        inputNodeReply.appendChild(document.createTextNode(''));
+
+        var buttonNode = document.createElement('button');
+        buttonNode.className = 'submitButton';
+        buttonNode.type = 'submit';
+        buttonNode.name = 'submitButton';
+        buttonNode.classList.add('btn', 'btn-primary', 'glyphicon', 'glyphicon-pencil', 'col-sm-2', 'col-sm-offset-1', 'col-xs-5', 'col-xs-offset-3');
+        buttonNode.appendChild(document.createTextNode('Svara'));
+
+
+        entry.addEventListener('submit', newReplyHandler);
+
+
+        // entry.appendChild(nameNode);
+        entry.appendChild(inputNodeName);
+        entry.appendChild(inputNodeReply);
+        entry.appendChild(buttonNode);
+
+        // entry.appendChild(replysNode);
+
+        return entry;
+        // console.log('create reoky form');
+        // var entry = document.createElement('form');
+        // entry.classList.add('reply');
+        //
+        // var nameInputNode = document.createElement('input');
+        // nameInputNode.classList.add('name');
+        // document.getElementsByName('name').placeholder = 'Nafn';
+        //
+        // return entry;
+    }
+
+    /**
+     * Býr til DOM tré fyrir form group.
+     *
+     * @param {string} label Label á form
+     * @param {string} name Nafn á input
+     *
+     * @return {node} DOM node fyrir form group
+     */
+    function createFormGroup(label, name) {}
+
+    /**
+     * Sækir færslur úr localStorage og skilar í fylki
+     *
+     * @param {string} key Lykill sem við notum í localStorage
+     * @param {array} Fylki af færslum til að vista
+     */
+    function save(key, items) {}
+
+    /**
+     * Sækir færslur úr localStorage og skilar í fylki
+     *
+     * @param {string} key Lykill sem við notum í localStorage
+     *
+     * @return {array} Fylki af færslum í localStorage ef einhverjar
+     * @return {null} Ef engin færsla til staðar í localStorage
+     */
+    function load(key) {}
+
+    return {
+        init: init
+    };
 })();
